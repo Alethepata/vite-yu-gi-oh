7f5f38df123636070ba74d707237823741652e2d
Alethepata <alessia.patane@gmail.com>
2023-10-12 16:21:52 +0200

    [Edit] cards

--- src/components/partials/CardsContainer.vue
+++ src/components/partials/CardsContainer.vue
@@ -1,16 +1,28 @@
 <script>
+import { store } from '../../data/store';
 import Cards from './Cards.vue';
 export default {
     name: 'CardsContainer',
     components: {
         Cards
+    },
+    data() {
+        return {
+            store
+        }
     }
 }
 </script>
 
 <template>
 
-    <cards/>
+    <cards
+    v-for="card in store.characterList"
+    :key="card.id"
+    :name="card.name"
+    :archetype="card.archetype"
+    :cardImg="card.card_images"
+    />
   
 </template>
 
