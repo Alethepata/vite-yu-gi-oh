92e6624e470f76a9a1b227a2266f2499fa668a39
Alethepata <alessia.patane@gmail.com>
2023-10-12 16:21:26 +0200

    [Add] props

--- src/components/partials/Cards.vue
+++ src/components/partials/Cards.vue
@@ -1,10 +1,25 @@
 <script>
 export default {
-    name:'Cards'
+    name: 'Cards',
+    props: {
+        name: String,
+        archetype: String,
+        cardImg:Object
+    }
 }
 </script>
 
 <template>
+
+    <img
+    v-for="img in cardImg"
+    :key="img.id"
+    :src="img.image_url" 
+    alt="">
+
+    <h1>{{ name }}</h1>
+
+    <p>{{ archetype }}</p>
   
 </template>
 
