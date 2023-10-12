3df4cb2f731c3c137e7ad9e0367a0028edc41b7a
Alethepata <alessia.patane@gmail.com>
2023-10-12 16:35:17 +0200

    [Add] logo / h1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,9 +5,19 @@ export default {
 </script>
 
 <template>
+
+    <header class=" d-flex py-2">
+        <div class="logo mx-5">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/800px-Yu-Gi-Oh%21_%28Logo%29.jpg" alt="">
+        </div>
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
   
 </template>
 
-<style>
+<style lang="scss" scoped>
+img{
+    height: 50px;
+}
 
 </style>
