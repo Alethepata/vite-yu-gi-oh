8c898ee1bd1e6fd7eb97f8e433709796577d0860
Alethepata <alessia.patane@gmail.com>
2023-10-12 15:21:32 +0200

    [Add] Main

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<script>
+import CardsContainer from './partials/CardsContainer.vue';
+export default {
+    name: 'Main',
+    components: {
+        CardsContainer
+    }
+}
+</script>
+
+<template>
+    <CardsContainer/>
+</template>
+
+<style>
+
+</style>
